03e9fd5027d28f7ca7a2f1692f5f60eeb5f771d6
Shiyang Lu <sl1642@westeros.cs.rutgers.edu>
2020-07-11 18:17:50 -0400

    add train.sh

--- run_3dmatch_benchmark.sh
+++ run_3dmatch_benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python -m scripts.benchmark_3dmatch \
-    --source ./dataset/threedmatch_test \
+    --source ./datasets/threedmatch_test \
     --target ./outputs \
     --voxel_size 0.025 \
     --model ./weights/2019-08-19_06-17-41.pth \
